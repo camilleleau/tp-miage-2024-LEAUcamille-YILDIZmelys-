f164c0e32ecb145c8142fa07ec6c076b0908d58f
camilleleau <camille.leau6@gmail.com>
2024-03-17 23:06:56 +0100

    réponse aux questions

--- todolist-debut-ex2/src/test/java/com/acme/todolist/ApplicationTests.java
+++ todolist-debut-ex2/src/test/java/com/acme/todolist/ApplicationTests.java
@@ -15,19 +15,19 @@ class ApplicationTests {
 	@Test
 	public void testFinalContentOnTime() throws Exception{
 		Instant time = Instant.now();
-		String content_call = "Manger du pain";
+		String content_call = "Never gonna give you up";
 		TodoItem todoItem = new TodoItem("this is ID !!!", time, content_call);
 
-		assertEquals( content_call, todoItem.finalContent(), "Le contenu devrait être identique si il n'a pas de retard");
+		assertEquals( content_call, todoItem.finalContent(), "Le message devrait être le même si il est dans le temps imparti");
 	}
 
 	@Test
 	public void testFinalContentLate() throws Exception{
 		Instant time = Instant.parse("2020-02-27T10:31:43Z");
-		String content_call = "Manger du bon lapin !!";
+		String content_call = "Never gonna give you up";
 		TodoItem todoItem = new TodoItem("0f8-06eb17ba8d34", time,content_call );
 
-		assertEquals("[LATE!] "+content_call, todoItem.finalContent(), "Le contenu devrait être différent si il y a du retard");
+		assertEquals("[LATE!] "+content_call, todoItem.finalContent(), "Le message devrait être différent si il y a dépassé le temps imparti");
 	}
 
 }
